d1f6c88856ef3e2476c1a2e5e4105dfc6f243388
Audrey Lelong <audrey.lelong@etu.u-bordeaux.fr>
2023-12-08 01:34:37 +0100

    cangement style wip

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,12 +1,28 @@
 <script setup>
 import NavBar from './components/NavBar.vue';
+import Style from './components/Style.vue';
+import './style.css';
+
 </script>
 
 <template>
-  <NavBar msg="test"/>
+  <NavBar/>
+  <select v-model="selected">
+  <option disabled value="">Choisissez</option>
+  <option>Style de Base</option>
+  <option>Noël</option>
+  <option>Horreur</option>
+  <option>Arc-en-ciel</option>
+</select>
+<div v-if="selected =='Noël'"></div>
   <router-view></router-view>
 </template>
 
 <style>
-@import './style.css';
+.dark-mode {
+    @import './style.css';
+}
+.light-mode {
+    @import './noel.css';
+}
 </style>
---
+++ front/src/components/Style.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+<select v-model="selected">
+  <option disabled value="">Choisissez</option>
+  <option>Style de Base</option>
+  <option>Noël</option>
+  <option>Horreur</option>
+  <option>Arc-en-ciel</option>
+</select>
+</template>
+
+<style scoped>
+</style>
